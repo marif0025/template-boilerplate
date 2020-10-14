72ceaccc8edb541f391a58515fa12b7025b7b15f
marif0025 <marif0025@gmail.com>
2020-10-14 17:38:06 +0500

    Template updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
-const path = require('path');
+const path = require('path')
+const Webpack = require('webpack')
 
-module.exports = {
-    entry: './src/js/index.js',
+module.exports = ({ mode }) => ({
+    mode,
+    devtool: mode === 'production' ? false : "eval-source-map",
+    entry: './src/ts/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
     output: {
+        publicPath: 'public/assets/js',
+        filename: 'script.js',
         path: path.resolve(__dirname, 'public/assets/js'),
-        filename: 'script.js'
-    }
-};
+        libraryTarget: 'umd',
+        library: 'uiLibrary',
+        libraryExport: 'default'
+    },
+    plugins: [
+        new Webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+    ],
+});
